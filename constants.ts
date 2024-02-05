2fd9402ff6299008f674f0c1796a83e274df55c5
Samyak Patni <samyak.patni@ninebit.in>
2024-02-05 17:54:31 +0530

    loginPage

---
+++ constants.ts
@@ -0,0 +1,20 @@
+
+export const mainHeading = 'WELCOME BACK';
+export const subHeading = 'Welcome back! Please enter your details.';
+export const email = 'Email';
+export const password = 'Password';
+export const rememberMeCheck = 'Remember me';
+export const forgotPasswordLink = 'Forgot password';
+export const signInBtn = 'Sign in';
+export const signInWithGoogleBtn = 'Sign in with Google';
+export const signUpText = 'Don’t have an account?';
+export const signUpLink = 'Sign up for free!';
+export const emailPlaceholder = 'Enter your email';
+export const passwordPlaceholder = '**********';
+export const loginSuccessMessage = 'SignIn Successful!';
+export const error401Message =
+  'Authentication Failed! Please enter correct credentials.';
+export const error404Message =
+  'We’re sorry, but the page you’re looking for is currently unavailable. Please try again later.';
+export const defaultErrorMessage =
+  'Something went wrong! Please try again later.';

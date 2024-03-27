693abb39f1c71d9621f0f1ea48fc57ba07338e1f
samyak-9bit <samyak.patni@ninebit.in>
2024-03-27 13:07:35 +0530

    Basic setup for web done

---
+++ src/App.tsx
@@ -0,0 +1,14 @@
+import React from 'react';
+import { Text, View } from 'react-native';
+
+
+
+function App() {
+  return (
+    <View>
+      <Text>gsdcugasduasu</Text>
+    </View>
+  );
+}
+
+export default App;

5271968702afde52dbad49202a4a1abcb7df75b2
samyak-9bit <samyak.patni@ninebit.in>
2024-03-28 10:36:19 +0530

    Removed unused files and folders

--- App.tsx
+++ App.tsx
@@ -1,38 +1,38 @@
-/**
- * Sample React Native App
- * https://github.com/facebook/react-native
- *
- * @format
- */
+// /**
+//  * Sample React Native App
+//  * https://github.com/facebook/react-native
+//  *
+//  * @format
+//  */
 
-import React from 'react';
-import {PaperProvider} from 'react-native-paper';
-import NewLoginPage from './components/NewLoginPage';
-import {NavigationContainer} from '@react-navigation/native';
-import {createNativeStackNavigator} from '@react-navigation/native-stack';
-// import LoginPage from './components/LoginPage';
-import Toast from 'react-native-toast-message';
-import LoggedInPage from './components/LoggedInPage';
-import RegistrationPage from './components/RegistrationPage';
+// import React from 'react';
+// import {PaperProvider} from 'react-native-paper';
+// // import NewLoginPage from './components/NewLoginPage';
+// import {NavigationContainer} from '@react-navigation/native';
+// import {createNativeStackNavigator} from '@react-navigation/native-stack';
+// // import LoginPage from './components/LoginPage';
+// import Toast from 'react-native-toast-message';
+// // import LoggedInPage from './components/LoggedInPage';
+// // import RegistrationPage from './components/RegistrationPage';
 
-const Stack = createNativeStackNavigator();
+// const Stack = createNativeStackNavigator();
 
-function App(): React.JSX.Element {
-  return (
-    <PaperProvider>
-      <NavigationContainer>
-        <Stack.Navigator screenOptions={{headerShown: false}}>
-          <Stack.Screen name="Login" component={NewLoginPage} />
-          <Stack.Screen name="Register" component={RegistrationPage} />
-          <Stack.Screen name="Welcome" component={LoggedInPage} />
-        </Stack.Navigator>
-        {/* <NewLoginPage /> */}
-        {/* <LoggedInPage /> */}
+// function App(): React.JSX.Element {
+//   return (
+//     <PaperProvider>
+//       {/* <NavigationContainer>
+//         {/* <Stack.Navigator screenOptions={{headerShown: false}}> */}
+//           {/* <Stack.Screen name="Login" component={NewLoginPage} />
+//           <Stack.Screen name="Register" component={RegistrationPage} />
+//           <Stack.Screen name="Welcome" component={LoggedInPage} /> */}
+//         </Stack.Navigator>
+//         {/* <NewLoginPage /> */}
+//         {/* <LoggedInPage /> */}
 
-        <Toast visibilityTime={3000} />
-      </NavigationContainer>
-    </PaperProvider>
-  );
-}
+//         <Toast visibilityTime={3000} />
+//       </NavigationContainer> */}
+//     </PaperProvider>
+//   );
+// }
 
-export default App;
+// export default App;
--- components/common Functions/ShowErrorToast.ts
+++
@@ -1,9 +0,0 @@
-/* eslint-disable prettier/prettier */
-import Toast from 'react-native-toast-message';
-
-export const showToast = (message: string) => {
-  Toast.show({
-    type: 'error',
-    text1: message,
-  });
-};

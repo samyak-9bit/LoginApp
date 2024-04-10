16c9ec0dadbd7e2cd2f9a15bdce7cf5a401123f8
samyak-9bit <samyak.patni@ninebit.in>
2024-04-10 17:55:12 +0530

    Sending chunks to node server

--- src/components/AudioStream.tsx
+++ src/components/AudioStream.tsx
@@ -1,102 +1,151 @@
-// import React, { useState } from 'react';
-// import { StyleSheet, TouchableOpacity, View} from 'react-native';
-// import { NavigationProp} from '@react-navigation/native';
-// import ImagePicker from 'react-native-image-picker'
-// import LiveAudioStream from 'react-native-live-audio-stream';
+//@ts-nocheck
+import React from 'react';
+import { StyleSheet, TouchableOpacity, View, Text} from 'react-native';
+import { NavigationProp} from '@react-navigation/native';
+import LiveAudioStream from 'react-native-live-audio-stream';
+import { Buffer } from 'buffer';
+import socketService from '../utils/socketService';
 
-// function AudioStream({ navigation }: { navigation: NavigationProp<any> }): React.JSX.Element {
+function AudioStream({ navigation }: { navigation: NavigationProp<any> }): React.JSX.Element {
 
-//     const options = {
-//         sampleRate: 32000,  // default is 44100 but 32000 is adequate for accurate voice recognition
-//         channels: 1,        // 1 or 2, default 1
-//         bitsPerSample: 16,  // 8 or 16, default 16
-//         audioSource: 6,     // android only (see below)
-//         bufferSize: 4096    // default is 2048
-//       };
+    const options = {
+        sampleRate: 32000,  // default is 44100 but 32000 is adequate for accurate voice recognition
+        channels: 1,        // 1 or 2, default 1
+        bitsPerSample: 16,  // 8 or 16, default 16
+        audioSource: 6,     // android only (see below)
+        bufferSize: 4096    // default is 2048
+      };
 
-//     LiveAudioStream.init(options);
-//     LiveAudioStream.on('data', data => {
-//       // base64-encoded audio data chunks
-//     });
-//   return ( 
-//     <View>
-//     </View>
-//   );
-// }
+    LiveAudioStream.init(options);
 
-// const styles = StyleSheet.create({
+    LiveAudioStream.on('data', data => {
+      var chunk = Buffer.from(data, 'base64');
+       //@ts-ignore
+      socketService.socket.emit('newData',chunk);
+      console.log("Chunk Created...");
+    });
 
-// });
-// export default AudioStream;
+    const startRecording=()=>{
+       //@ts-ignore
+      socketService.socket.emit('startRecording');
+      LiveAudioStream.start();
+    }
+
+    const stopRecording=()=>{
+      LiveAudioStream.stop();
+       //@ts-ignore
+      socketService.socket.emit('stopRecording');
+    }
+
+    return (
+      <View style={styles.container}>
+        <TouchableOpacity style={styles.button} onPress={()=>startRecording()}><Text>Start</Text></TouchableOpacity>
+        <TouchableOpacity style={[styles.button, styles.stopButton]} onPress={()=>stopRecording()}><Text>Stop</Text></TouchableOpacity>
+      </View>
+    );
+  }
+  
+  const styles = StyleSheet.create({
+    container: {
+      flex: 1,
+      alignItems: 'center',
+      justifyContent: 'center',
+    },
+    button: {
+      backgroundColor: '#3498db',
+      padding: 10,
+      borderRadius: 5,
+      marginVertical: 10,
+    },
+    stopButton: {
+      backgroundColor: '#e74c3c',
+    },
+  });
+
+export default AudioStream;
+
+
+
 //@ts-nocheck
 // 
 
 
-import React from 'react';
-import { View, Button, Platform } from 'react-native';
-import { launchImageLibrary } from 'react-native-image-picker';
-import Video from 'react-native-video';
+// import React from 'react';
+// import { View, Button, Platform } from 'react-native';
+// import { launchCamera, launchImageLibrary } from 'react-native-image-picker';
+// import Video from 'react-native-video';
 
 
-const SERVER_URL = 'http://10.0.2.2:3000';
+// const SERVER_URL = 'http://10.0.2.2:3000';
 
-const createFormData = (video, body = {}) => {
-  const data = new FormData();
+// const createFormData = (video, body = {}) => {
+//   const data = new FormData();
 
-  data.append('video', {
-    name: video.assets[0].fileName,
-    type: video.assets[0].type,
-    uri: Platform.OS === 'ios' ? video.uri.replace('file://', '') : video.assets[0].uri,
-  });
+//   data.append('video', {
+//     name: video.assets[0].fileName,
+//     type: video.assets[0].type,
+//     uri: Platform.OS === 'ios' ? video.uri.replace('file://', '') : video.assets[0].uri,
+//   });
 
-  Object.keys(body).forEach((key) => {
-    data.append(key, body[key]);
-  });
+//   Object.keys(body).forEach((key) => {
+//     data.append(key, body[key]);
+//   });
 
-  return data;
-};
-const AudioStream = () => {
-  const [video, setVideo] = React.useState(null);
+//   return data;
+// };
+// const AudioStream = () => {
+//   const [video, setVideo] = React.useState(null);
 
-  const handleChooseVideo = () => {
-    launchImageLibrary({ noData: true, mediaType:'video' }, (response) => {
-      console.log('response after selecting from gallery:',response);
-      if (response) {
-        setVideo(response);
-        console.log('Video state after selecting from gallery:',video);
-      }
-    });
-  };
+//   const handleChooseVideo = () => {
+//     launchImageLibrary({ noData: true, mediaType:'video' }, (response) => {
+//       console.log('response after selecting from gallery:',response);
+//       if (response) {
+//         setVideo(response);
+//         console.log('Video state after selecting from gallery:',video);
+//       }
+//     });
+//   };
 
-  const handleUploadVideo = () => {
-    fetch(`${SERVER_URL}/api/upload`, {
-      method: 'POST',
-      body: createFormData(video, { userId: '123' }),
-    })
-      .then((response) => response.json())
-      .then((response) => {
-        console.log('response', response);
-      })
-      .catch((error) => {
-        console.log('error', error);
-      });
-  };
+//   const handleCamera = () =>{
+//     launchCamera({ noData: true, mediaType:'video' }, (response) => {
+//         console.log('response after selecting from gallery:',response);
+//         if (response) {
+//           setVideo(response);
+//           console.log('Video state after selecting from gallery:',video);
+//         }
+//       });
+//   }
 
-  return (
-    <View style={{ flex: 1, alignItems: 'center', justifyContent: 'center' }}>
-      {video && (
-        <>
-          {/* <Video
-                source={{ uri: video.uri }}
-                style={{ flex: 1 }}
-                controls
-              /> */}
-          <Button title="Upload Video" onPress={handleUploadVideo} />
-        </>
-      )}
-      <Button title="Choose Video" onPress={handleChooseVideo} />
-    </View>
-  );
-};
+//   const handleUploadVideo = () => {
+//     fetch(`${SERVER_URL}/api/upload`, {
+//       method: 'POST',
+//       body: createFormData(video, { userId: '123' }),
+//     })
+//       .then((response) => response.json())
+//       .then((response) => {
+//         console.log('response', response);
+//       })
+//       .catch((error) => {
+//         console.log('error', error);
+//       });
+//   };
 
-export default AudioStream;
+//   return (
+//     <View style={{ flex: 1, alignItems: 'center', justifyContent: 'center' }}>
+//       {video && (
+//         <>
+//           {/* <Video
+//                 source={{ uri: video.uri }}
+//                 style={{ flex: 1 }}
+//                 controls
+//               /> */}
+//           <Button title="Upload Video" onPress={handleUploadVideo} />
+//         </>
+//       )}
+//       <Button title="Choose Video" onPress={handleChooseVideo} />
+//       <Button title="Record now" onPress={handleCamera} />
+//     </View>
+//   );
+// };
+
+// export default AudioStream;
